dfd5f63c219073b355629f74ea94ef2750dd6511
Lazizjon <lazizjondusmurodov@gmail.com>
2026-05-06 19:07:43 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+# ello.py
+# Lazizjonning birinchi Python projecti
+
+def main():
+    print("Salom, bu mening birinchi GitHub projectim!")
+    print("Men Python va GitHub bilan ishlashni o‘rganayapman.")
+
+if __name__ == "__main__":
+    main()
