9007a633951a36ec1c8902e84611267050372c82
Lazizjon <lazizjondusmurodov@gmail.com>
2026-05-16 11:59:19 +0500

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,9 +1,47 @@
-# ello.py
-# Lazizjonning birinchi Python projecti
+import sqlite3
 
-def main():
-    print("Salom, bu mening birinchi GitHub projectim!")
-    print("Men Python va GitHub bilan ishlashni o‘rganayapman.")
 
-if __name__ == "__main__":
-    main()
+conn = sqlite3.connect("any.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    age INTEGER
+)
+""")
+
+conn.commit()
+
+cursor.execute(
+    "INSERT INTO users(name, age) VALUES(?, ?)",
+    ("Laziz", 20)
+)
+
+conn.commit()
+
+cursor.execute("SELECT * FROM users")
+
+users = cursor.fetchall()
+
+print("Users:")
+for user in users:
+    print(user)
+
+cursor.execute(
+    "UPDATE users SET age=? WHERE id=?",
+    (25, 1)
+)
+
+conn.commit()
+
+cursor.execute(
+    "DELETE FROM users WHERE id=?",
+    (1,)
+)
+
+conn.commit()
+
+conn.close()
